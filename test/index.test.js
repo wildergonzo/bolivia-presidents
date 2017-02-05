8c694be8ec26a502f6972b345982770edbb29799
Wilder Gonzales <wilder.gonzales@gmail.com>
2017-02-06 00:17:52 +0100

    adding unit tests

---
+++ test/index.test.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect;
+var presidents = require('../src/index.js');
+
+describe('bolivia-presidents', function(){
+
+	describe('names', function(){
+		it('should be an array of strings', function(){
+			expect(presidents.names).to.satisfy(isArrayOfStrings);
+
+			function isArrayOfStrings(array){
+				return array.every(function(item){
+					return typeof item === 'string';
+				});
+			}
+		});
+
+		it('should include `Carlos D. Mesa Gisbert`', function(){
+			expect(presidents.names).to.include('Carlos D. Mesa Gisbert');
+		});
+	});
+	
+	describe('random', function(){
+		it('should return a random item from the presidents.all', function(){
+			var randomItem = presidents.random;
+			expect(presidents.all).to.include(randomItem);
+		});
+	});
+
+});
